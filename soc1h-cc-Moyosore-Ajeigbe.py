dba8951eb70349273fbf9a86e50bae842c9ee5b2
Moyosore Ajeigbe <41216003+Moyosore15@users.noreply.github.com>
2018-07-17 09:10:17 -0400

    Create soc1h-cc-Moyosore-Ajeigbe.py

---
+++ soc1h-cc-Moyosore-Ajeigbe.py
@@ -0,0 +1,13 @@
+''' 
+hours_in_day = 24 
+days_in_year= 365
+hours_in_year= hours_in_day*days_in_year; 
+print (hours_in_year); #number of hours in a year
+minutes_in_decade = hours_in_year * 10 * 60;
+print (minutes_in_decade); #number of minutes in a decade
+seconds_in_year= hours_in_year*360;
+Age_in_seconds= 23*seconds_in_year; 
+print (Age_in_seconds); #my age in seconds 
+Age = 72532800/seconds_in_year
+print (Age)
+'''
